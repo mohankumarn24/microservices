fae45396212dd31d6de44a1c184bcf64e9300ee5
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-08-08 14:42:24 +0530

    initial commit

---
+++ 1-Microservice(S1-S10-LBsetup/1-eureka-service-8761/src/main/java/com/infybuzz/app/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.infybuzz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
+
+/*
+http://localhost:8761/
+http://localhost:8080/api/student/getById/1
+ */
---
+++ README.md
@@ -0,0 +1,2 @@
+# microservices
+Course notes (https://www.udemy.com/course/microservices-with-java-spring-boot-spring-cloud-eureka-api-gateway/)
